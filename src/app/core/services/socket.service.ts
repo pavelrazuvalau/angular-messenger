f0840cc7b115ffc563a0f079385e1b92408e9e5e
Pavel Razuvalau <pavel_razuvalau@epam.com>
2022-04-13 17:13:48 +0300

    feat: add socket.io

---
+++ src/app/core/services/socket.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import io from 'socket.io-client';
+import Socket = SocketIOClient.Socket;
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  socket: Socket | null = null;
+
+  connect(token: string) {
+    this.disconnect();
+
+    this.socket = io(environment.apiUrl, { query: { auth_token: token } });
+
+    this.socket?.on('message', (message: any) => {
+      console.log(message);
+    });
+  }
+
+  disconnect() {
+    this.socket?.disconnect();
+  }
+}
